d29962f030abde12015e6a7b19a282b03cad69f7
League Student <league@AdministorsiMac.attlocal.net>
2019-06-27 14:25:21 -0700

    Day 4

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -5,7 +5,11 @@ import java.util.Random;
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
-
+public static void main(String[] args) {
+System.out.println();
+	
+	String number= "new random().nextInt(4)";
+}
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
 
 	// 3. Print out this variable
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,26 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+int score= 0;
 		// 2.  Ask the user a question 
-		
+String answer=JOptionPane.showInputDialog("Who plays Pam in the Office?");		
 		// 3.  Use an if statement to check if their answer is correct
-		
+if(answer.equals("Jenna Fischer")){ JOptionPane.showMessageDialog(null,"Nice Job You Are Correct!");
+}else JOptionPane.showMessageDialog(null,"Sorry, You Are Incorrect");
 		// 4.  if the user's answer was correct, add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
-	}
+if(answer.equals("Jenna Fischer")){ System.out.println("You have 1 point!");
+}else System.out.println("You have 0 points");
 }
+		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+	
+		// 6.  After all the questions have been asked, print the user's score 
+	
+	}
+
+
